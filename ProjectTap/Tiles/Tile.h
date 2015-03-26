c0bbd5d6108e079dafc94fdb0354a23cdc2cbbb0
ポケレゲミッシングナムバー <pokelegemissingno@gmail.com>
2015-03-26 13:58:06 -0600

    start tile base class

---
+++ ProjectTap/Tiles/Tile.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "Tile.generated.h"
+
+UCLASS()
+class PROJECTTAP_API ATile : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ATile();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick( float DeltaSeconds ) override;
+};

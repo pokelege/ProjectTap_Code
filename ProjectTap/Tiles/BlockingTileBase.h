6589755c023d63e622a1f344a6214df70e44dc9c
wozhendeaa <billyyang520@gmail.com>
2015-04-15 17:36:29 -0600

    do

---
+++ ProjectTap/Tiles/BlockingTileBase.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Tile.h"
+#include "BlockingTileBase.generated.h"
+
+UCLASS()
+class PROJECTTAP_API ABlockingTileBase : public ATile
+{
+	GENERATED_BODY()
+	
+public:	
+
+	// Sets default values for this actor's properties
+	ABlockingTileBase();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick(float DeltaSeconds)override;
+
+	
+	
+};

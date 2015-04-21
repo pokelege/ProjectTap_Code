d00b0935b6e8afb70695528e6d8340d4c27a83ee
ポケレゲ <pokelegemissingno@gmail.com>
2015-04-21 10:25:57 -0600

    ramp

--- ProjectTap/Tiles/Ramp.cpp
+++ ProjectTap/Tiles/Ramp.cpp
@@ -21,16 +21,51 @@ ARamp::ARamp(): ATile(  )
 		BoxCollision->AddLocalOffset(FVector(0, 0, -10));
 	}
 
+	if(boxTrigger == nullptr)
+	{
+		boxTrigger = CreateDefaultSubobject<UBoxComponent>(TEXT("Ramp box trigger"));
+		boxTrigger->AttachTo(this->GetRootComponent());
+		boxTrigger->SetBoxExtent(FVector(0.5f,0.5f,2.0f), false);
+	}
+
+	boxTrigger->SetCollisionResponseToAllChannels(ECR_Ignore);
+	boxTrigger->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	boxTrigger->bGenerateOverlapEvents = true;
+	boxTrigger->SetNotifyRigidBodyCollision(false);
+	boxTrigger->SetCollisionObjectType(ECollisionChannel::ECC_WorldDynamic);
+	boxTrigger->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECR_Overlap);
+	pawnIn.BindUFunction(this, "OnBeginTriggerOverlap");
+	boxTrigger->OnComponentBeginOverlap.Add(pawnIn);
+	pawnOut.BindUFunction(this, "OnEndTriggerOverlap");
+	boxTrigger->OnComponentEndOverlap.Add(pawnOut);
 	ConstructorHelpers::FObjectFinder<UCurveFloat> curve(*RAMP_CURVE_PATH.ToString());
 	if(curve.Object != nullptr) rotationSequence = curve.Object;
 
 	auto pc = Cast<UPrimitiveComponent>(RootComponent);
 	pc->SetWorldScale3D(FVector(40.0f, 40.0f, 2.0f));
+
+	forceMultiplier = 1000.0f;
 }
 
 void ARamp::BeginPlay()
 {
 	Super::BeginPlay();
+	UPrimitiveComponent* pc = Cast<UPrimitiveComponent>(RootComponent);
+	switch(rotationDirection)
+	{
+		case Direction::XPlus:
+			pc->SetWorldRotation(FRotator(0,0,0));
+			break;
+		case Direction::xMinus:
+			pc->SetWorldRotation(FRotator(0,180,0));
+			break;
+		case Direction::YPlus:
+			pc->SetWorldRotation(FRotator(0,90,0));
+			break;
+		case Direction::yMinus:
+			pc->SetWorldRotation(FRotator(0,270,0));
+			break;
+	}
 }
 
 void ARamp::Tick(float DeltaTime)
@@ -72,7 +107,37 @@ void ARamp::BoostBall()
 
 void ARamp::activate()
 {
-	if(rotationSequence == nullptr || activated) return;
+	if(rotationSequence == nullptr || ball == nullptr || activated) return;
 	Super::activate();
 	time = 0;
+	switch(rotationDirection)
+	{
+		case Direction::XPlus:
+			ball->AddVelocity(forceMultiplier * FVector(1,0,0));
+			break;
+		case Direction::xMinus:
+			ball->AddVelocity(forceMultiplier * FVector(-1,0,0));
+			break;
+		case Direction::YPlus:
+			ball->AddVelocity(forceMultiplier * FVector(0,1,0));
+			break;
+		case Direction::yMinus:
+			ball->AddVelocity(forceMultiplier * FVector(0,-1,0));
+			break;
+	}
+}
+
+void ARamp::OnBeginTriggerOverlap(AActor* OtherActor,
+						   UPrimitiveComponent* OtherComp,
+						   int32 OtherBodyIndex,
+						   bool bFromSweep,
+						   const FHitResult & SweepResult)
+{
+	ball = Cast<ABallPawn>(OtherActor);
+}
+void ARamp::OnEndTriggerOverlap(AActor* OtherActor,
+						 UPrimitiveComponent* OtherComp,
+						 int32 OtherBodyIndex)
+{
+	if(Cast<ABallPawn>(OtherActor) != nullptr) ball = nullptr;
 }
--- ProjectTap/Tiles/Ramp.h
+++ ProjectTap/Tiles/Ramp.h
@@ -5,6 +5,16 @@
 #include "Tiles/Tile.h"
 #include "Pawns/BallPawn.h"
 #include "Ramp.generated.h"
+
+UENUM(BlueprintType)
+enum class Direction : uint8
+{
+	XPlus,
+	YPlus,
+	xMinus,
+	yMinus
+};
+
 /**
  * 
  */
@@ -12,13 +22,21 @@ UCLASS()
 class PROJECTTAP_API ARamp : public ATile
 {
 	GENERATED_BODY()
+
 	static const FName RAMP_MESH_PATH;
 	static const FName RAMP_CURVE_PATH;
-	
+	FScriptDelegate pawnIn;
+	FScriptDelegate pawnOut;
 	float time = 0;
+	ABallPawn* ball;
 public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Tile)
+	Direction rotationDirection;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Tile)
 	UCurveFloat* rotationSequence;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Tile)
+	UBoxComponent* boxTrigger;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Tile)
 	float forceMultiplier;
@@ -31,6 +49,14 @@ public:
 	virtual void activate() override;
 
 	void BoostBall();
-
-
+	UFUNCTION()
+	void OnBeginTriggerOverlap(AActor* OtherActor,
+						  UPrimitiveComponent* OtherComp,
+					   int32 OtherBodyIndex,
+					   bool bFromSweep,
+					   const FHitResult & SweepResult);
+	UFUNCTION()
+	void OnEndTriggerOverlap(AActor* OtherActor,
+							   UPrimitiveComponent* OtherComp,
+							int32 OtherBodyIndex);
 };

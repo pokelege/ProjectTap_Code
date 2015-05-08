488bbafad940c9198215ace2764727d2d3f41583
ポケレゲ <pokelegemissingno@gmail.com>
2015-05-07 18:40:14 -0600

    magnet can slowly pull

--- ProjectTap/Tiles/MagnetTile.cpp
+++ ProjectTap/Tiles/MagnetTile.cpp
@@ -1,26 +1,40 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "ProjectTap.h"
+#include "Pawns/BallPawn.h"
 #include "MagnetTile.h"
 
 
-
+AMagnetTile::AMagnetTile() : ATile()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	ConstructorHelpers::FObjectFinder<UStaticMesh> mesh(*FName("/Game/Models/Magnet").ToString());
+	TileMesh->SetStaticMesh(mesh.Object);
+	BoxCollision->SetBoxExtent(FVector(1.0f));
+	auto pc = Cast<UPrimitiveComponent>(RootComponent);
+	pc->SetWorldScale3D(FVector(40.0f, 40.0f, 40.0f));
+}
 
 void AMagnetTile::Tick( float DeltaTime )
 {
-// 	Super::Tick(DeltaTime);
-// 	FHitResult hit;
-// 	FCollisionQueryParams queryParam;
-// 	queryParam.bFindInitialOverlaps = false;
-// 	queryParam.bReturnFaceIndex = true;
-// 	FCollisionObjectQueryParams objectParam = objectParam.DefaultObjectQueryParam;
-//
-// 	auto pos = GetActorLocation();
-// 	auto rayStart = pos + dir * 2.0f;
-// 	auto laserVector = dir * length;
-// 	auto laserEmitter = laserParticle->EmitterInstances[0];
-//
-// 	GetWorld()->LineTraceSingle(hit,rayStart, pos + laserVector, queryParam, objectParam);
-// 	auto hitActor = hit.Actor.Get();
-//
+	Super::Tick(DeltaTime);
+	FHitResult hit;
+	FCollisionQueryParams queryParam;
+	queryParam.bFindInitialOverlaps = false;
+	queryParam.bReturnFaceIndex = true;
+	FCollisionObjectQueryParams objectParam = objectParam.DefaultObjectQueryParam;
+
+	//todo use socket instead
+	auto pos = TileMesh->GetSocketLocation("Socket");
+	auto rayStart = pos + GetActorForwardVector() * 2.0f;
+	auto laserVector = GetActorForwardVector() * length;
+	//auto laserEmitter = laserParticle->EmitterInstances[0];
+
+	GetWorld()->LineTraceSingle(hit,rayStart, pos + laserVector, queryParam, objectParam);
+	auto hitActor = hit.Actor.Get();
+	ABallPawn* pawn;
+	if((pawn = Cast<ABallPawn>(hitActor)) != nullptr)
+	{
+		pawn->AddVelocity(-GetActorForwardVector() * 1000.0f, false);
+	}
 }
--- ProjectTap/Tiles/MagnetTile.h
+++ ProjectTap/Tiles/MagnetTile.h
@@ -6,14 +6,19 @@
 #include "MagnetTile.generated.h"
 
 /**
- * 
+ *
  */
 UCLASS()
 class PROJECTTAP_API AMagnetTile : public ATile
 {
 	GENERATED_BODY()
-	
-	
-	
+
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Magnet)
+	float length = 99999.0f;
+
+	AMagnetTile();
 	virtual void Tick( float DeltaTime ) override;
+
 };

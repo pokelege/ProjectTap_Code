b9ff2fa698baee9c53e9c7a6f8bead31accff60f
yyang <billyyang520@gmail.com>
2015-07-07 17:21:41 -0600

    builds

--- ProjectTap/ProjectTap.Build.cs
+++ ProjectTap/ProjectTap.Build.cs
@@ -8,7 +8,7 @@ public class ProjectTap : ModuleRules
 	{
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore","AIModule", "UMG" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+        PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
